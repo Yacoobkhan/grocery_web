4ac96704d41fd4477ed0105e22b342946bb2fcd8
Yacoobkhan <161142322+Yacoobkhan@users.noreply.github.com>
2026-05-05 08:03:56 +0530

    Update server.js

--- server/backend/server.js
+++ server/backend/server.js
@@ -18,7 +18,7 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://grocery-web-client.onrender.com", "https://grocery-web-admin.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
