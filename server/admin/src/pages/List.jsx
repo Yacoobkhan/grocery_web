b5de686024036f062d415d2ef773c1f6c1ccdee9
Yacoobkhan <161142322+Yacoobkhan@users.noreply.github.com>
2026-05-05 08:30:23 +0530

    Update List.jsx

--- server/admin/src/pages/List.jsx
+++ server/admin/src/pages/List.jsx
@@ -14,7 +14,7 @@ const List = ({ token }) => {
       const response = await axios.get(
         backendUrl + "/api/product/list"
       );
-
+      console.log("FINAL API:", `${backendUrl}/api/product/list`);
       if (response.data.success) {
         setList(response.data.products.reverse());
       } else {
